083f60d51b2cf2d1ccb4df0f90c0d257c76c3aff
git-harshIIIT <107746453+git-harshIIIT@users.noreply.github.com>
2024-01-24 21:45:47 +0000

    video 1 and 2

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const id = 1234
+let address = "Varanasi"
+var name = "Harshit"
+let roll;
+
+// id = 2345  // can't change value of a const variable
+console.log(id)
+console.log(roll) //undefined until its un-defined
+console.table([id,address,name,roll])
+
+// don't use var as it isn't a scope term
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+//number
+let age = 22
+
+//bigint
+//symbol
+
+//string
+let name =  "Harshit"
+
+// boolean
+let isOpen = true
+
+//NULL
+let val = null;
+
+//undefined
+let x ;
+
+console.table([typeof(age),typeof(isOpen),typeof(val),typeof(x),typeof(name)])
