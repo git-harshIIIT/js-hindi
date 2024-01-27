21f209bfeb1841acf2fbba8ceb588afe93635311
git-harshIIIT <107746453+git-harshIIIT@users.noreply.github.com>
2024-01-27 13:26:02 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,45 @@
+// let score = null;
+// let valueInNumber = Number(score);
+
+// console.log(typeof(score));
+// console.log(valueInNumber);
+// console.log(typeof(valueInNumber));
+
+// typecasting to number:-
+// 33 -> number
+// "33" -> number
+// "21abc" -> NaN
+// true -> 1
+
+
+
+
+
+
+// let isLoggedIn = undefined;
+
+// console.log(typeof(isLoggedIn));
+// let boolLoggedIn = Boolean(isLoggedIn);
+// console.log(boolLoggedIn);
+// console.log(typeof(boolLoggedIn));
+
+//typecasting to boolean:-
+//0 - false
+//1 - true
+// "" - false
+// "harshit" - true
+// null - false 
+// undefined - false
+
+
+
+
+
+
+
+
+
+
+
+
+
